dbf14b12952944f9d7a13998c33c2073cf899d48
devtvas <tarcisio.word@gmail.com>
2022-04-28 00:29:26 -0300

    ft_custom-appbar - svgimage - adicionado novas dependencias

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,21 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:live_coding_dio_podcast/app/util/custom_btn_category_widget.dart';
+import 'package:live_coding_dio_podcast/app/util/custom_color.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Podcast Dio'),
-      ),
-      body: const Center(
-        child: Text(
-          'Digital Innovation One',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.red,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Stack(
+                      alignment: Alignment.centerRight,
+                      children: [
+                        SvgPicture.asset("assets/logo.svg",
+                            width: 40, height: 40),
+                      ],
+                    ),
+                    Row(
+                      children: const [
+                        Text(
+                          "Dio",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w800),
+                        ),
+                        Text(
+                          "Cast",
+                          style: TextStyle(
+                              color: CustomColor.pink700,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
